85acad34daa1b28e58f95effd742e4f126efcc74
Mary <mary.farinha@gmail.com>
2015-12-03 20:42:24 +0000

    feat: Add SQL tournament database schema and partially completed tournament.py
    First version of the SQL tournament Database Schema added to the repository.
    Functions connect, deleteMatches, deletePlayers and countPlayers were implemented in tournament.py

---
+++ tournament.sql
@@ -0,0 +1,31 @@
+-- Table definitions for the tournament project.
+--
+-- Put your SQL 'create table' statements in this file; also 'create view'
+-- statements if you choose to use it.
+--
+-- You can write comments in this file by starting them with two dashes, like
+-- these lines here.
+
+CREATE DATABASE tournament;
+
+\c tournament;
+
+
+CREATE TABLE players( id SERIAL PRIMARY KEY,
+                      name TEXT NOT NULL);
+
+CREATE TABLE enrollments( sport TEXT NOT NULL,
+                          player_id INTEGER REFERENCES players(id),
+						  PRIMARY KEY(sport, player_id));
+
+CREATE TYPE outcome AS ENUM('win','lose','draw');
+
+CREATE TABLE games( sport TEXT NOT NULL,
+                    player INTEGER REFERENCES players(id),
+					opponent INTEGER REFERENCES players(id),
+					outcome outcome NOT NULL,
+					PRIMARY KEY(sport,player,opponent),
+					FOREIGN KEY(sport,player) REFERENCES enrollments (sport, player_id),
+					FOREIGN KEY(sport,opponent) REFERENCES enrollments (sport, player_id),
+					CHECK (player != opponent));
+
